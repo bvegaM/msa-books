dafade819730d15c6112cdd43e04ec38e6e67d1a
bvegam <vegabryam40@gmail.com>
2025-06-13 18:22:40 -0500

    init commit

---
+++ src/main/java/com/gdg/book/domain/Author.java
@@ -0,0 +1,20 @@
+package com.gdg.book.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Author {
+
+  private String id;
+  private String name;
+  private String email;
+  private String bio;
+}
